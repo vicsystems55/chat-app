dca88e84f2d2e8af17fd4e87b5264fc877996978
vicSystems <victorasuquob@gmail.com>
2021-10-10 14:49:37 +0100

    code update

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MessageController extends Controller
+{
+    //
+
+    public function messages()
+    {
+        
+        
+        return $messages;
+    }
+
+    public function create_message(Request $request)
+    {
+        
+        
+        
+        return $message;
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MessageController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::group(['middleware' => ['auth:sanctum']], function(){
+
+    Route::get('/messages', [MessageController::class, 'messages']);
+
+    Route::get('/create_message', [MessageController::class, 'create_message']);
+
+});
